fa2b3ac0d9c368d1fc8f8a470e5f2f7ece4f9569
Miriam Calcia <calcia.dev@gmail.com>
2021-02-25 23:45:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+"""
+Conditional Structure;
+Performing a check.
+"""
+
+# Variable declaration
+x = 1
+y = 50.000
+
+#verification block
+if x> y:
+    print ('x is greater than y')
+if y> x:
+    print ('y is greater than x')
+
+"""Realize that in the verification block, the chosen condition (x>y) is false, so only line 15 of the code will be executed
+"""
